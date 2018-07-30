34b94db14c13570a3fda9cdedaecfc06aeccff04
Mephistofer <Mephistof@163.com>
2018-07-30 23:20:50 +0800

    dd

---
+++ JSCode/playlist.js
@@ -0,0 +1,17 @@
+var drink = "shekaka";
+var lyrics = "";
+var cans = 3;
+while(cans > 0) {
+	lyrics = lyrics + cans + " can of " + drink + " on the wall <br>";
+	if (cans > 1) {
+		lyrics = lyrics + (cans-1) + " cans of " + drink + " on the wall <br>";
+	}
+	else {
+		lyrics = lyrics + "No more cans of " + drink + " on the wall <br>";
+	}
+	cans = cans -1;
+	//alert("cans = "+cans);
+	//document.write("asdasd" + "<br>");
+}
+
+document.write(lyrics);
