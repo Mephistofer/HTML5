14541be13c28643bd862b1460af69d8ed8486728
Mephistofer <Mephistof@163.com>
2018-08-06 21:39:05 +0800

    sss

--- JSCode/playlist.js
+++ JSCode/playlist.js
@@ -1,15 +1,18 @@
-var drink = "shekaka";
-var lyrics = "";
-var cans = 3;
-while(cans > 0) {
-	lyrics = lyrics + cans + " can of " + drink + " on the wall <br>";
-	if (cans > 1) {
-		lyrics = lyrics + (cans-1) + " cans of " + drink + " on the wall <br>";
+window.onload = init;
+
+function init() {
+	var button = document.getElementById("addButton");
+	button.onclick = handleButtonClick;
+}
+
+function handleButtonClick() {
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+	if (songName == "") {
+		alert("Please enter a song");
 	}
 	else {
-		lyrics = lyrics + "No more cans of " + drink + " on the wall <br>";
+		alert("Adding " + songName);
 	}
-	cans = cans -1;
 }
-abc
-document.write(lyrics);
+
